878ce0ff0e321906d1d2356a278fe5e06a215b51
RandyKdev <rkwalar@gmail.com>
2021-11-23 18:45:47 +0100

    Add documentation for empty window

--- empty_window.c
+++ empty_window.c
@@ -1,12 +1,21 @@
 #include <gtk/gtk.h>
 
 int main(int argc, char **argv) {
+    // initializes gtk
     gtk_init(&argc, &argv);
 
+    // defining a widget
     GtkWidget *window;
+    
+    // creates new window - GTK_WINDOW_TOPLEVEL to indicate it is not a popup
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    
+    // binds a callback to the close button of the widget
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
+    // show widget to screen
     gtk_widget_show_all(window);
     
+    // calls the main instance of gtk
     gtk_main();
 }
