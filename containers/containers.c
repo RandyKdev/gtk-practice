9805246e8f72b9c29460f10618c4fa0027bc58c2
RandyKdev <rkwalar@gmail.com>
2021-11-24 14:59:14 +0100

    Add how to remove a widget from container and add another

--- containers/containers.c
+++ containers/containers.c
@@ -6,6 +6,7 @@ int main(int argc, char **argv) {
 
     // defining a pointer to a button widget
     GtkWidget *button;
+    GtkWidget *button1;
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
@@ -13,9 +14,15 @@ int main(int argc, char **argv) {
 
     // creating a button with label "click"
     button = gtk_button_new_with_label("Click");
+    button1 = gtk_button_new_with_label("Click 2");
     
     // Adding the button to the window
     gtk_container_add(GTK_CONTAINER(window), button);
+    
+    // removing a widget from window
+    gtk_container_remove(GTK_CONTAINER(window), button);
+
+    gtk_container_add(GTK_CONTAINER(window), button1);
 
     gtk_widget_show_all(window);
 
